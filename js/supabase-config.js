928d15d5de52a806a2a3ba58c50ca8d060201ba8
clashroyalito29-netizen <clashroyalito29@gmail.com>
2026-01-14 20:39:43 -0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,4 +4,4 @@ const SUPABASE_URL = "https://eabpagvszxbxhzxiciyx.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhYnBhZ3ZzenhieGh6eGljaXl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MjQxMjIsImV4cCI6MjA4NDAwMDEyMn0.O0ACM1pw1VQN-2WcEcqryWCCs7psW1MUF8T8CmHa9Io";
 
 
-window.supabase = supabaseClient;
+
