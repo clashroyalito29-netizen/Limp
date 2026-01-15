bdfef87433fb564a98db4715c29fb513c4809687
clashroyalito29-netizen <clashroyalito29@gmail.com>
2026-01-14 22:19:58 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,16 @@
+async function compressAndUpload(file) {
+    const options = {
+        maxSizeMB: 0.8,          // Máximo 800KB
+        maxWidthOrHeight: 1024, // Redimensiona si es muy grande
+        useWebWorker: true
+    };
+    
+    try {
+        const compressedFile = await imageCompression(file, options);
+        console.log(`Comprimido: de ${file.size/1024}KB a ${compressedFile.size/1024}KB`);
+        return compressedFile; // Este es el que mandás a Supabase Storage
+    } catch (error) {
+        console.error("Error comprimiendo:", error);
+    }
+}
 
